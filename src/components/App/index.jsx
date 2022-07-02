720f6a9b6851c263a5f1c2184d7cfabe2f8238c7
=maazjab <=>
2022-07-02 16:42:56 +0200

    Feat routes in App

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Routes, Route} from  'react-router-dom';
 //Components imports
 import Header from '../Header';
 import Footer from '../Footer';
 import Landing from '../Landing';
+import Login from '../Login';
+import Signup from '../Signup';
+import Welcome from '../Welcome';
 //CSS imports
 import '../../App.css';
+import ErrorPage from '../ErrorPage';
+
 const App = () => {
   return (
     <div className="App">
         <Header/>
-        <Landing/>
+         <Router>
+            <Routes>
+              <Route path="/" element={<Landing />}/>
+              <Route path="/welcome"  element={<Welcome />}/>
+              <Route path="/login"  element={<Login />}/>
+              <Route path="/signup"  element={<Signup />}/>
+              <Route path="/error-page"  element={<ErrorPage />}/>
+            </Routes>
+         </Router>
         <Footer/>
     </div>
   )
